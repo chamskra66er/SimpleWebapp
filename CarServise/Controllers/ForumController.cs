0e5771377289788aa1db50b5c8276389ee13ac6b
Anatoliy Novikov <chamskra66er@gmail.com>
2020-02-14 14:32:19 +0300

    added forum view, controller

---
+++ CarServise/Controllers/ForumController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarServise.Data;
+using CarServise.Data.Models;
+using CarServise.Models.ForumViewModels;
+
+namespace CarServise.Controllers
+{
+    public class ForumController:Controller
+    {
+        private readonly IForum _forumService;
+        public ForumController(IForum forumService)
+        {
+            _forumService = forumService;
+        }
+        public IActionResult Index()
+        {
+            var forums = _forumService.GeyAll()
+                .Select(forum=> new ForumListingModel
+                {
+                    Id=forum.Id,
+                    Name=forum.Title,
+                    Value = forum.Value
+                });
+            var model = new ForumIndexModel
+            {
+                ForumList = forums
+            };
+
+            return View(model);
+        }
+
+    }
+}
---
+++ CarServise/Models/ForumViewModels/ForumListingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarServise.Models.ForumViewModels
+{
+    public class ForumListingModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
