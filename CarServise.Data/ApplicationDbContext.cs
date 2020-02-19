75c027e5d29411bdc5dd6a971ddb8f7d26720d00
Anatoliy Novikov <chamskra66er@gmail.com>
2020-02-19 21:23:34 +0300

    start create AdminRole

--- CarServise.Data/ApplicationDbContext.cs
+++ CarServise.Data/ApplicationDbContext.cs
@@ -2,6 +2,11 @@
 using Microsoft.EntityFrameworkCore;
 using CarServise.Models;
 using CarServise.Data.Models;
+using System.Threading.Tasks;
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CarServise.Data
 {
@@ -10,6 +15,15 @@ namespace CarServise.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Forum> Forums { get; set; }
+
+        public static async Task CreateAdminAccount(IServiceProvider serviceProvider, 
+            IConfiguration configuration)
+        {
+            UserManager<ApplicationUser> userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            RoleManager<IdentityRole> roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string username = configuration["Data:AdminUser:Name"];
+        }
 
     }
 }
