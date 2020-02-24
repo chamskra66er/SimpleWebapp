4b86cf8e89eebdffe8119ad4678b3aa1a3173d01
Anatoliy Novikov <chamskra66er@gmail.com>
2020-02-24 14:06:10 +0300

    added NavigationMenu view component

---
+++ CarServise/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarServise.Components
+{
+    public class NavigationMenuViewComponent:ViewComponent
+    {
+        public string Invoke() => "TODO Navigate";
+    }
+}
