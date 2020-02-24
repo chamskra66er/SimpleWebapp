8feedd6b51a2eeebaff9d5459b1e910bd6f21cb8
Anatoliy Novikov <chamskra66er@gmail.com>
2020-02-24 14:48:48 +0300

    finish navMenu from db

--- CarServise/Components/NavigationMenuViewComponent.cs
+++ CarServise/Components/NavigationMenuViewComponent.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarServise.Data;
+using CarServise.Models.ForumViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,23 @@ namespace CarServise.Components
 {
     public class NavigationMenuViewComponent:ViewComponent
     {
-        public string Invoke() => "TODO Navigate";
+        private readonly IForum _forumService;
+        public NavigationMenuViewComponent(IForum forumService)
+        {
+            _forumService = forumService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var forums = _forumService.GetAll()
+                .Select(forum => new ForumListingModel
+                {
+                    Id = forum.Id,
+                    Name = forum.Title,
+                });
+            return View(forums
+                .Select(x=>x.Name)
+                .Distinct()
+                .OrderBy(x=>x));
+        }
     }
 }
