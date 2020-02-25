870c5c0aeb3191bab6835d166e06b8e0e1c1a1d0
Anatoliy Novikov <chamskra66er@gmail.com>
2020-02-25 22:52:38 +0300

    added ICart, CartService, changed Cart

---
+++ CarServise.Data/ICart.cs
@@ -0,0 +1,17 @@
+﻿using CarServise.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarServise.Data
+{
+    public interface ICart
+    {
+        void AddItem(Forum forum, int quantity);
+        void RemoveLine(Forum forum);
+        decimal ComputerTotalValue();
+        void Clear();
+        IEnumerable<Cart> Lines();
+
+    }
+}
---
+++ CarServise.Service/CartService.cs
@@ -0,0 +1,42 @@
+﻿using CarServise.Data;
+using CarServise.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarServise.Service
+{
+    public class CartService : ICart
+    {
+        private List<Cart> lineCollection = new List<Cart>();
+        public void AddItem(Forum forum, int quantity)
+        {
+            Cart line = lineCollection
+                .Where(p => p.Forum.Id == forum.Id)
+                .FirstOrDefault();
+            if (line == null)
+            {
+                lineCollection.Add(new Cart
+                {
+                    Forum = forum,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public virtual void Clear() => lineCollection.Clear();
+
+        public virtual decimal ComputerTotalValue() =>
+            lineCollection.Sum(e => Convert.ToInt32(e.Forum.Value) * e.Quantity);
+
+        public virtual IEnumerable<Cart> Lines() => lineCollection;
+
+        public virtual void RemoveLine(Forum forum) =>
+            lineCollection.RemoveAll(l => l.Forum.Id == forum.Id);
+    }
+}
