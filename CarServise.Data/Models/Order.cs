effd2e526f169084f192484c2c58ff017db499c1
Anatoliy Novikov <chamskra66er@gmail.com>
2020-03-05 14:26:01 +0300

    added Order model for db

---
+++ CarServise.Data/Models/Order.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CarServise.Data.Models
+{
+    public class Order
+    {
+        [BindNever]
+        public int OrderId { get; set; }
+        [BindNever]
+        public ICollection<CartLine> Lines { get; set; }
+        [BindNever]
+        public string ShipStatus { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалйуста, введите адрес")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите название города")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Пожалйуста, введите название района")]
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите название страны")]
+        public string Country { get; set; }
+
+        public string Deliver { get; set; }
+    }
+}
