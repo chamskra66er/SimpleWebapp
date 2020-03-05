411b8737b66cab36194eb5c52186ffab5cf7eafb
Anatoliy Novikov <chamskra66er@gmail.com>
2020-03-05 14:54:16 +0300

    create intarface, service order

---
+++ CarServise.Data/IOrder.cs
@@ -0,0 +1,12 @@
+﻿using CarServise.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarServise.Data
+{
+    public interface IOrder
+    {
+        IQueryable<Order> Orders { get; }
+        void SaveOrder(Order order);
+    }
+}
---
+++ CarServise.Service/OrderService.cs
@@ -0,0 +1,32 @@
+﻿using CarServise.Data;
+using CarServise.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarServise.Service
+{
+    public class OrderService : IOrder
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Order> Orders =>
+            _context.Orders.Include(l => l.Lines).ThenInclude(o => o.Forum);
+
+        public void SaveOrder(Order order)
+        {
+            _context.AttachRange(order.Lines.Select(l => l.Forum));
+            if (order.OrderId == 0)
+            {
+                _context.Orders.Add(order);
+            }
+            _context.SaveChanges();
+        }
+    }
+}
